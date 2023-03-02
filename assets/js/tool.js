05fb790ca8e0909e48259ed62cf3f690b702da23
John Tokarowski <Johnt3@bu.edu>
2023-03-02 15:13:54 +0000

    changes

--- assets/js/tool.js
+++ assets/js/tool.js
@@ -250,7 +250,7 @@
     function downloadData() {
 
         console.log("API Request initiated")
-        jsonData = '{requested_series_identifier_list: [{"series_identifier": "EXHOSLUSM495S","fill_methodology": "interpolate"},{"series_identifier": "CPHPTT01EZM659N","fill_methodology": "interpolate"},{"series_identifier": "HOUST","fill_methodology": "interpolate"}], "target_frequency": "D"}'
+        jsonData = '{"requested_series_identifier_list": [{"series_identifier": "EXHOSLUSM495S","fill_methodology": "interpolate"},{"series_identifier": "CPHPTT01EZM659N","fill_methodology": "interpolate"},{"series_identifier": "HOUST","fill_methodology": "interpolate"}], "target_frequency": "D"}'
 
 
         $.ajax({
